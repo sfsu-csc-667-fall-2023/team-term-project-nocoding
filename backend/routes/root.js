72cbec78479e88be89c84d27abcfb58146222433
karlMoreno <karlcmoreno0811@gmail.com>
2023-10-27 17:36:07 -0700

    db connection routes

--- backend/routes/root.js
+++ backend/routes/root.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '1234',
+  database: 'GameDB'
+});
+
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('MySQL Connected.');
+});
+
+
+
 router.get("/:name", (request, response) => {
     const { name } = request.params; // Use request.params, not response.params
     response.render('root', { name });
 });
+
+
 
 module.exports = router;
